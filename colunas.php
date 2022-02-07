a332ecbd5786d70c691e5e01398b70275c48d35c
marcosboni <74115335+marcosboni@users.noreply.github.com>
2022-02-07 19:49:59 -0300

    Add files via upload

---
+++ colunas.php
@@ -0,0 +1,7 @@
+<div class="section-colunas">
+   <div class="grid-1"></div>
+   <div class="grid-2"></div>
+   <div class="grid-3"></div>
+   <div class="grid-4"></div>
+   <div class="grid-5"></div>
+</div>
